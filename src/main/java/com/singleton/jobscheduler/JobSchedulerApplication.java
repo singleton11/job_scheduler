d8a39fc21620bd8ef816cd363a837655ac440fe6
Anton Prokhorov <betrayer11@gmail.com>
2018-08-24 21:29:03 +0300

    Initial commit

---
+++ src/main/java/com/singleton/jobscheduler/JobSchedulerApplication.java
@@ -0,0 +1,12 @@
+package com.singleton.jobscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSchedulerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JobSchedulerApplication.class, args);
+  }
+}
